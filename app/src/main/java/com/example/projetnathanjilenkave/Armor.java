d0a7974531dd61814581e4ba22102a38f771c79d
nathanremond <99425832+nathanremond@users.noreply.github.com>
2025-04-10 11:15:43 +0200

    feat: Création classe Object + ajout paramètre price (#6)

--- app/src/main/java/com/example/projetnathanjilenkave/Armor.java
+++ app/src/main/java/com/example/projetnathanjilenkave/Armor.java
@@ -3,10 +3,13 @@ package com.example.projetnathanjilenkave;
 public class Armor extends Equipment{
 
     int bonusDefense;
+    int price;
 
-    public Armor(String name, String rarity, int bonusDefense) {
+
+    public Armor(String name, String rarity, int bonusDefense, int price) {
         super(name, rarity);
         this.bonusDefense = bonusDefense;
+        this.price = price;
     }
 
     public int getBonusDefense() {
@@ -15,5 +18,13 @@ public class Armor extends Equipment{
 
     public void setBonusDefense(int bonusDefense) {
         this.bonusDefense = bonusDefense;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
