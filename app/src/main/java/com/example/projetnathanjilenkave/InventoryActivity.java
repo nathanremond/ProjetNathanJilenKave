989774782ebd83b6e217705a6dea19a93c1a416f
nathanremond <perso.nathan.remond@gmail.com>
2025-04-17 17:58:33 +0200

    fix: Suppression de code inutile dans la page inventaire

--- app/src/main/java/com/example/projetnathanjilenkave/InventoryActivity.java
+++ app/src/main/java/com/example/projetnathanjilenkave/InventoryActivity.java
@@ -16,23 +16,7 @@ public class InventoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
 
-        //Affichage des stats
         displayplayerStats();
-
-        SharedPreferences sharedPreferencesWeapon = getSharedPreferences("playerWeapon", MODE_PRIVATE);
-        String nomArmeJoueur = sharedPreferencesWeapon.getString("nomArmeJoueur", "DefaultWeapon");
-        String raretéArmeJoueur = sharedPreferencesWeapon.getString("raretéArmeJoueur", "DefaultRarityWeapon");
-        int bonusStrengthArmeJoueur = sharedPreferencesWeapon.getInt("bonusStrengthArmeJoueur", 0);
-
-        displayCharaWeapon(nomArmeJoueur, raretéArmeJoueur , bonusStrengthArmeJoueur);
-
-        SharedPreferences sharedPreferencesArmor = getSharedPreferences("playerArmor", MODE_PRIVATE);
-        String nomArmureJoueur = sharedPreferencesArmor.getString("nomArmureJoueur", "DefaultArmor");
-        String raretéArmureJoueur = sharedPreferencesArmor.getString("raretéArmureJoueur", "DefaultRarityArmor");
-        int bonusDefenseArmureJoueur = sharedPreferencesArmor.getInt("bonusDefenseArmureJoueur", 0);
-
-        displayCharaArmor(nomArmureJoueur, raretéArmureJoueur , bonusDefenseArmureJoueur);
-
 
         buttonNull();
         //Fermer l'inventaire
@@ -40,7 +24,6 @@ public class InventoryActivity extends AppCompatActivity {
         closeBtn.setOnClickListener(view -> {
             finish();
         });
-
     }
 
     private void buttonNull() {
@@ -98,25 +81,5 @@ public class InventoryActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.armorName)).setText("Armure : " + player.getArmorName());
         ((TextView) findViewById(R.id.armorDefense)).setText("Défense : " + player.getArmorDefense());
         ((TextView) findViewById(R.id.armorRarity)).setText("Rareté : " + player.getArmorRarity());
-    }
-
-    private void displayCharaWeapon(String nomArmeJoueur, String raretéArmeJoueur , int bonusStrengthArmeJoueur){
-        TextView weaponNamePlayer = findViewById(R.id.WeaponName);
-        TextView rarityWeaponPlayer = findViewById(R.id.WeaponRarity);
-        TextView bonusStrengthWeapon = findViewById(R.id.WeaponBonusStat);
-
-        weaponNamePlayer.setText(nomArmeJoueur);
-        rarityWeaponPlayer.setText(raretéArmeJoueur);
-        bonusStrengthWeapon.setText(String.valueOf(bonusStrengthArmeJoueur));
-    }
-
-    private void displayCharaArmor(String nomArmureJoueur, String raretéArmureJoueur , int bonusDefenseArmureJoueur){
-        TextView armorNamePlayer = findViewById(R.id.ArmorName);
-        TextView rarityArmorPlayer = findViewById(R.id.ArmorRarity);
-        TextView bonusDefenseArmor = findViewById(R.id.ArmorBonusStat);
-
-        armorNamePlayer.setText(nomArmureJoueur);
-        rarityArmorPlayer.setText(raretéArmureJoueur);
-        bonusDefenseArmor.setText(String.valueOf(bonusDefenseArmureJoueur));
     }
 }
